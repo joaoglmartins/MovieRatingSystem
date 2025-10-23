4ae189a62286cf9271af3a24c03bb13a9b84fdaa
João Martins <joaogasparlopesmartins@gmail.com>
2025-10-23 10:36:25 +0100

    fix: remove main method from Customer

--- src/Customer.java
+++ src/Customer.java
@@ -2,11 +2,6 @@
 public class Customer {
 	String movieTitle;
 	int rating;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public Customer(String movieName, int movieRating) {
 		this.movieTitle = movieName;
